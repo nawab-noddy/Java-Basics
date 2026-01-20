2bb7acb21b9ac54b5fbd46bee277629cf86c19f7
Mohammad Anas <mohammadanas8948@gmail.com>
2026-01-20 13:35:30 +0530

    Mini Project 2 Understand the Constructor Topic

---
+++ Basic Java/src/BankAccount.java
@@ -0,0 +1,47 @@
+
+public class BankAccount {
+
+    private int accountNumber;
+    private String holderName;
+    private int balance;
+
+
+//    CONSTRUCTORS
+    public BankAccount(int accountNumber, String holderName, int balance){
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+//    Deposit Method
+    public void deposit(int amount) throws IllegalArgumentException {
+        if(amount < 0){
+            throw new IllegalArgumentException("Please Enter amount Greater than 0");
+        }
+        if(amount == 0){
+            throw new IllegalArgumentException("Please Enter amount Greater than 0");
+        }
+        this.balance += amount;
+    }
+
+//    Withdraw Method
+    public void withdraw(int amount) throws IllegalArgumentException{
+        if(amount < 0){
+         throw new IllegalArgumentException("Please Enter a valid amount to withdraw");
+        }
+        if(amount > this.balance){
+            throw new IllegalArgumentException("Please Enter a valid amount to withdraw");
+        }
+        if(amount == 0){
+            throw new IllegalArgumentException("Please Enter a valid amount to withdraw");
+        }
+        this.balance -= amount;
+    }
+
+//    Check Balance
+
+    public int getBalance(){
+        return this.balance;
+    }
+
+}
