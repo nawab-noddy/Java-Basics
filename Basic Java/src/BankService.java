0719affaad3f7562ba5b60dc781a419696cc233c
Mohammad Anas <mohammadanas8948@gmail.com>
2026-01-27 15:55:53 +0530

    createAccount complete

--- Basic Java/src/BankService.java
+++ Basic Java/src/BankService.java
@@ -1,15 +1,17 @@
 import java.util.HashMap;
 
 class BankService{
-    private HashMap<Integer,String> storeAccounts = new HashMap<>();
+
+    private final HashMap<Integer,BankAccount> storeAccounts = new HashMap<>();
 
 //    CREATE ACCOUNT
-    public void createAccount(int accNumber) throws IllegalArgumentException{
+    public void createAccount(int accNumber, String holderName, int initialBalance) throws IllegalArgumentException{
         if(storeAccounts.containsKey(accNumber)){
             throw new IllegalArgumentException("Account Already Existed");
         }
         else {
-
+            BankAccount newAccount = new BankAccount(accNumber, holderName, initialBalance);
+            storeAccounts.put(accNumber,newAccount);
         }
     }
 }
